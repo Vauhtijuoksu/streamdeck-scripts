a5c6b5fe11677d235236d953459cc570c7d5bab5
Aleksi Seppänen <aleksi@seppanen.dev>
2023-04-29 11:29:49 +0300

    Support for multiple timers

--- timer.py
+++ timer.py
@@ -4,15 +4,19 @@ import datetime
 import requests
 import json
 
+timers = {
+    "1": "6a9a5188-980c-4845-9841-650b5006c7f0",
+    "2": "726de5ee-686a-4ce4-8a90-6df2012e9aea"
+}
 
-def patchStreamMetadata(config, value):
+def patchTimer(config, value, id):
     api_url = config['VAUHTIJUOKSU_API_URL']
-    r = requests.patch(f'{api_url}/stream-metadata/', json=value, auth=(config['BASIC_AUTH_USER'], config['BASIC_AUTH_PW']))
+    r = requests.patch(f'{api_url}/timers/{id}', json=value, auth=(config['BASIC_AUTH_USER'], config['BASIC_AUTH_PW']))
     if r.status_code == 200:
         return json.loads(r.content)
     else:
         print(r.status_code)
-        print(r.content)
+        print(r.text)
 
 
 def getTimestamp():
@@ -33,42 +37,63 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    if args.option == 'start':
-        patchStreamMetadata(config, {
-            'timers':[
-                {
-                    'start_time': getTimestamp(),
-                    'end_time': None
-                }
-            ]
-            })
+    if args.option == 'start1':
+        patchTimer(config, {
+                'start_time': getTimestamp(),
+                'end_time': None
+            }, timers['1'])
 
-    elif args.option == 'continue':
-        patchStreamMetadata(config, {
-            'timers':[
-                {
-                    'end_time': None
-                }
-            ]
-            })
+    elif args.option == 'start2':
+        patchTimer(config, {
+                'start_time': getTimestamp(),
+                'end_time': None
+            }, timers['2'])
 
-    elif args.option == 'stop':
-        patchStreamMetadata(config, {
-            'timers':[
-                {
-                    'end_time': getTimestamp(),
+    elif args.option == 'startboth':
+        patchTimer(config, {
+                'start_time': getTimestamp(),
+                'end_time': None
+            }, timers['1'])
+        patchTimer(config, {
+                'start_time': getTimestamp(),
+                'end_time': None
+            }, timers['2'])
 
-                }
-            ]
-            })
-    elif args.option == 'reset':
-        patchStreamMetadata(config, {
-            'timers':[
-                {
-                    'start_time': None,
-                    'end_time': None
-                }
-            ]
-            })
+    elif args.option == 'continue1':
+        patchTimer(config, {
+                'end_time': None
+            }, timers['1'])
+
+    elif args.option == 'continue2':
+        patchTimer(config, {
+                'end_time': None
+            }, timers['2'])
+    elif args.option == 'stop1':
+        patchTimer(config, {
+            'end_time': getTimestamp()
+            }, timers['1'])
+    elif args.option == 'stop2':
+        patchTimer(config, {
+            'end_time': getTimestamp()
+            }, timers['2'])
+    elif args.option == 'reset1':
+        patchTimer(config, {
+                'start_time': None,
+                'end_time': None
+            }, timers['1'])
+    elif args.option == 'reset2':
+        patchTimer(config, {
+                'start_time': None,
+                'end_time': None
+            }, timers['2'])
+    elif args.option == 'resetboth':
+        patchTimer(config, {
+                'start_time': None,
+                'end_time': None
+            }, timers['1'])
+        patchTimer(config, {
+                'start_time': None,
+                'end_time': None
+            }, timers['2'])
     else:
         print('invalid option')
